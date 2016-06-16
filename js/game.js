70e1ea03c18a782e8f532973d619963c641d9864
akosua kernizan <akernizan@gmail.com>
2016-06-16 18:09:40 -0400

    fixed bh growth function

--- js/game.js
+++ js/game.js
@@ -3,7 +3,10 @@ var SCORETXT;
 var stars;
 var star;
 
-var game = new Phaser.Game('100%', '100%', Phaser.AUTO, 'wrapper', {
+var d = 50;
+var circleD = (d * 2)
+
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'wrapper', {
 	preload: preload,
 	create: create,
 	update: update
@@ -31,10 +34,10 @@ function create() {
 	game.stage.backgroundColor = '#7ec0ee';
 	cling = this.game.add.audio('cling');
 
-	var graphics = game.add.graphics(10, 10);
+	graphics = game.add.graphics(10, 10);
 	graphics.lineStyle(0);
 	graphics.beginFill('#222222', 1);
-	graphics.drawCircle(game.world.centerX, game.world.centerY, 50);
+	graphics.drawCircle(game.world.centerX, game.world.centerY, d);
 	graphics.endFill();
 
 	window.graphics = graphics;
@@ -44,7 +47,7 @@ function create() {
 	stars.inputEnabled = true;
 	//	stars.body.allowGravity = true;
 	//	starArray = [star, star2, star3, star4, star5, star6, star7, star8];
-	//
+
 	star = stars.create(Math.floor(Math.random() * 300), 200, 'star');
 	star2 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 400), 'star');
 	star3 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 450), 'star');
@@ -53,7 +56,8 @@ function create() {
 	star6 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 300), 'star');
 	star7 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 600), 'star');
 	star8 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 344), 'star');
-
+	star9 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 444), 'star');
+	star10 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 304), 'star');
 
 	star.lifespan = 3000;
 	star2.lifespan = 3000;
@@ -63,16 +67,9 @@ function create() {
 	star6.lifespan = 3000;
 	star7.lifespan = 3000;
 	star8.lifespan = 3000;
+	star9.lifespan = 3000;
+	star10.lifespan = 3000;
 
-
-	star.inputEnabled = true;
-	star2.inputEnabled = true;
-	star3.inputEnabled = true;
-	star4.inputEnabled = true;
-	star5.inputEnabled = true;
-	star6.inputEnabled = true;
-	star7.inputEnabled = true;
-	star8.inputEnabled = true;
 
 	star.events.onInputDown.add(starClick, this);
 	star2.events.onInputDown.add(starClick, this);
@@ -82,6 +79,19 @@ function create() {
 	star6.events.onInputDown.add(starClick, this);
 	star7.events.onInputDown.add(starClick, this);
 	star8.events.onInputDown.add(starClick, this);
+	star9.events.onInputDown.add(starClick, this);
+	star10.events.onInputDown.add(starClick, this);
+
+	star.inputEnabled = true;
+	star2.inputEnabled = true;
+	star3.inputEnabled = true;
+	star4.inputEnabled = true;
+	star5.inputEnabled = true;
+	star6.inputEnabled = true;
+	star7.inputEnabled = true;
+	star8.inputEnabled = true;
+	star9.inputEnabled = true;
+	star10.inputEnabled = true;
 
 
 }
@@ -100,11 +110,13 @@ starInt = setInterval(function () {
 	star = stars.create(Math.floor(Math.random() * 300), 200, 'star');
 	star2 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 400), 'star');
 	star3 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 450), 'star');
-	star4 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 250), 'star');
+	star4 = stars.create(Math.floor(Math.random() * 700), Math.floor(Math.random() * 250), 'star');
 	star5 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 350), 'star');
-	star6 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 300), 'star');
+	star6 = stars.create(Math.floor(Math.random() * 800), Math.floor(Math.random() * 300), 'star');
 	star7 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 600), 'star');
-	star8 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 344), 'star');
+	star8 = stars.create(Math.floor(Math.random() * 300), Math.floor(Math.random() * 344), 'star');
+	star9 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 444), 'star');
+	star10 = stars.create(Math.floor(Math.random() * 500), Math.floor(Math.random() * 304), 'star');
 
 	star.lifespan = 3000;
 	star2.lifespan = 3000;
@@ -114,6 +126,8 @@ starInt = setInterval(function () {
 	star6.lifespan = 3000;
 	star7.lifespan = 3000;
 	star8.lifespan = 3000;
+	star9.lifespan = 3000;
+	star10.lifespan = 3000;
 
 
 	star.events.onInputDown.add(starClick, this);
@@ -124,6 +138,8 @@ starInt = setInterval(function () {
 	star6.events.onInputDown.add(starClick, this);
 	star7.events.onInputDown.add(starClick, this);
 	star8.events.onInputDown.add(starClick, this);
+	star9.events.onInputDown.add(starClick, this);
+	star10.events.onInputDown.add(starClick, this);
 
 	star.inputEnabled = true;
 	star2.inputEnabled = true;
@@ -133,6 +149,8 @@ starInt = setInterval(function () {
 	star6.inputEnabled = true;
 	star7.inputEnabled = true;
 	star8.inputEnabled = true;
+	star9.inputEnabled = true;
+	star10.inputEnabled = true;
 }, 3000)
 
 
@@ -143,7 +161,39 @@ function starClick(star, pointer) {
 	SCORETXT.text = 'Score: ' + SCORE;
 }
 
+function nextLevel() {
+	if (SCORE == 500) {
+		alert('Next')
+	}
+}
+
+function growBH() {
+
+	graphics = game.add.graphics(10, 10);
+	graphics.lineStyle(0);
+	graphics.beginFill('#222222', 1);
+	graphics.drawCircle(game.world.centerX, game.world.centerY, circleD);
+	graphics.endFill();
+
+	console.log(circleD);
+
+
+}
+
+bhInt = setInterval(function () {
+	//	growBH();
+
+	//
+	//	var i = 0;
+	//
+	graphics = game.add.graphics(10, 10);
+	graphics.lineStyle(0);
+	graphics.beginFill('#222222', 1);
+	graphics.drawCircle(game.world.centerX, game.world.centerY, (circleD += 50));
+	graphics.endFill();
+
+}, 1000)
+
 function update() {
-
-
+	//	nextLevel();
 }
